784a643ed96baa5f7e001b6b6203fa8246f48660
lebol2018 <44235113+lebol2018@users.noreply.github.com>
2018-12-03 09:57:01 +0100

    Add files via upload

---
+++ score.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+import time
+import pickle
+
+# Function to calculate the RMSE score
+def calculate_score(m1, m2, verbose=True):
+    '''
+    Input: m1 - Either the test or train utility matrix
+           m2 - The predictions matrix
+           verbose - True/False whether or not to output text during the calculation
+
+    Output: rmse - The calculated Root Mean Squared Error
+    '''
+    if verbose:
+        print("Calculating score...")
+        print("Find diff...")
+
+    # First subtract the matrices
+    # m1 contains lots if NaNs, meaning we only actually get the diff between the
+    # original ratings and the predictions
+    diff = np.subtract(m1, m2)
+
+    if verbose:
+        print("Done.")
+        print("fillna...")
+
+    # Replace NaNs with zeroes
+    diff = diff.fillna(0)
+
+    if verbose:
+        print("Done.")
+        print("Find sqaure of diff...")
+
+    # Find the square of the diffs
+    diff = diff*diff
+
+    if verbose:
+        print("Done.")
+        print("Sum of diff...")
+
+    # Find the sum of the (squared) diffs
+    err = np.sum(np.sum(diff))
+
+    if verbose:
+        print("Done.")
+
+    # Divide by the number of non-zero elements
+    # We need to divide 'err' by the number of original ratings
+    if verbose:
+        print("Count number of non-zero values...")
+    d = diff[diff == 0].count().sum()
+    d = diff.shape[0]*diff.shape[1] - d
+    mse = err/d
+    rmse = np.sqrt(mse)
+    return rmse
+
+print("Load training matrix from file...")
+with open('user_ratings_train.dat', 'rb') as fp:
+    train_mat = pickle.load(fp)
+print("Done.")
+print(train_mat.shape)
+
+print("Load test matrix from file...")
+with open('user_ratings_test.dat', 'rb') as fp:
+    test_mat = pickle.load(fp)
+print("Done.")
+print(test_mat.shape)
+
+print("Load predictions matrix from file...")
+with open('predictions_matrix.dat', 'rb') as fp:
+    preds = pickle.load(fp)
+print("Done.")
+print(preds.shape)
+
+# Calculate MSE taking nans into account
+train_rmse = calculate_score(train_mat, preds)
+print("RMSE on training set: " + str(train_rmse))
+test_rmse = calculate_score(test_mat, preds)
+print("RMSE on test set: " + str(test_rmse))
